970bbe03a3cc12f0d5dcd8f7e3987eb60f3eec36
Dancho28bg <y.rusev28@gmail.com>
2024-11-18 12:14:28 +0100

    Navigation Bar Prototype

---
+++ Client/src/components/Navbar.tsx
@@ -0,0 +1,26 @@
+﻿import React from 'react';
+import styles from './NavBar.module.css';
+
+const NavBar: React.FC = () => {
+    return (
+        <div className={styles.nav}>
+            {/* Logo */}
+            <div className={styles.logo}>
+                <img src="/images/logo.png" alt="logo" />
+            </div>
+
+            {/* Center Buttons */}
+            <ul className={styles.navButtons}>
+                <li className={styles.navItem}>Play</li>
+                <li className={styles.navItem}>Board History</li>
+                <li className={styles.navItem}>Admin Panel</li>
+                <li className={styles.navItem}>Current Winnings</li>
+            </ul>
+
+            {/* Profile Button */}
+            <div className={styles.profileButton}>Profile</div>
+        </div>
+    );
+};
+
+export default NavBar;
