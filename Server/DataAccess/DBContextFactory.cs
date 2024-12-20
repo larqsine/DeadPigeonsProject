b8b3f38097617f67813a6aff39f6e1205a2ce126
Dancho28bg <y.rusev28@gmail.com>
2024-12-20 04:51:25 +0100

    accidentaly deleted class

---
+++ Server/DataAccess/DBContextFactory.cs
@@ -0,0 +1,30 @@
+﻿/*using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace DataAccess
+{
+
+    public class DBContextFactory : IDesignTimeDbContextFactory<DBContext>
+    {
+        public DBContext CreateDbContext(string[] args)
+        {
+            // Adjust the path to locate the appsettings.json in the API folder
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../API"))
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // Retrieve the connection string
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            // Configure DbContextOptions
+            var optionsBuilder = new DbContextOptionsBuilder<DBContext>();
+            optionsBuilder.UseNpgsql(connectionString);
+
+            return new DBContext(optionsBuilder.Options);
+        }
+    }
+
+}*/
